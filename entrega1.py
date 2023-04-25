7a921424e1e33cf60714de024245fa0e4f43f3aa
pablofontanessi <pablofontanessi@gmail.com>
2023-04-25 19:17:45 -0300

    refactor

--- entrega1.py
+++ entrega1.py
@@ -1,4 +1,3 @@
-
 from simpleai.search import (
     SearchProblem,
     breadth_first,
@@ -14,94 +13,81 @@ from simpleai.search.viewers import WebViewer, BaseViewer
 
 def jugar(paredes, cajas, objetivos, jugador, maximos_movimientos):
 
-    # coordenadas paredes
-    PAREDES = paredes
-
-    # coordenada global objetivos
-    OBJETIVOS = objetivos
-
-    #Maximos movimientos
-    MaxMov = maximos_movimientos
-
     # listado de acciones disponibles
     ACTIONS = (
-    'arriba',
-    'abajo',
-    'izquierda',
-    'derecha',
+        'arriba',
+        'abajo',
+        'izquierda',
+        'derecha',
     )
     
-    def distanciaObjetivoCercano(box):
-        min_distance = float('inf')
-        for goal in OBJETIVOS:
-            distance = abs(box[0] - goal[0]) + abs(box[1] - goal[1])
-            if distance < min_distance:
-                min_distance = distance
-        return min_distance
+    def distancia_objetivo_cercano(box):
+        distancia_minima = float('inf')
+
+        for objetivo in objetivos:
+            distance = abs(box[0] - objetivo[0]) + abs(box[1] - objetivo[1])
+            if distance < distancia_minima:
+                distancia_minima = distance
+
+        return distancia_minima
 
 
-    def movimientoValido(jugador, accion, boxes):
-        nueva_coord = None
+    def movimiento_valido(jugador, accion, boxes):
+        nueva_coordenada = None
        
         # Calcular la nueva coordenada del jugador segun la accion
         if accion == 'abajo':
-            nueva_coord = (jugador[0] + 1, jugador[1])
+            nueva_coordenada = (jugador[0] + 1, jugador[1])
         elif accion == 'arriba':
-            nueva_coord = (jugador[0] - 1, jugador[1])
+            nueva_coordenada = (jugador[0] - 1, jugador[1])
         elif accion == 'derecha':
-            nueva_coord = (jugador[0], jugador[1] + 1)
+            nueva_coordenada = (jugador[0], jugador[1] + 1)
         elif accion == 'izquierda':
-            nueva_coord = (jugador[0], jugador[1] - 1)
+            nueva_coordenada = (jugador[0], jugador[1] - 1)
 
         # Verificar si la nueva coordenada del jugador es una pared
-        if nueva_coord in PAREDES:
+        if nueva_coordenada in paredes:
             return False
 
         # Si la nueva coordenada del jugador hace mover una caja, verificar si la caja se puede mover en la misma dirección
-        if nueva_coord in boxes:
-            nueva_coord_caja = None
+        if nueva_coordenada in boxes:
+            nueva_coordenada_caja = None
             if accion == 'abajo':
-                nueva_coord_caja = (nueva_coord[0] + 1, nueva_coord[1])
+                nueva_coordenada_caja = (nueva_coordenada[0] + 1, nueva_coordenada[1])
             elif accion == 'arriba':
-                nueva_coord_caja = (nueva_coord[0] - 1, nueva_coord[1])
+                nueva_coordenada_caja = (nueva_coordenada[0] - 1, nueva_coordenada[1])
             elif accion == 'derecha':
-                nueva_coord_caja = (nueva_coord[0], nueva_coord[1] + 1)
+                nueva_coordenada_caja = (nueva_coordenada[0], nueva_coordenada[1] + 1)
             elif accion == 'izquierda':
-                nueva_coord_caja = (nueva_coord[0], nueva_coord[1] - 1)
+                nueva_coordenada_caja = (nueva_coordenada[0], nueva_coordenada[1] - 1)
 
             # Verificar si la nueva coordenada de la caja es una pared o tiene una caja adyacente
-            if nueva_coord_caja in PAREDES or nueva_coord_caja in boxes:
+            if nueva_coordenada_caja in paredes or nueva_coordenada_caja in boxes:
                 return False
 
         # Si la nueva coordenada del jugador no es una pared ni una caja, es un movimiento válido
         return True
 
-    def nuevaCoord(jugador, accion):
-        nueva_coord = None
+    def nueva_coordenada(jugador, accion):
+        nueva_coordenada = None
         # Calcular la nueva coordenada del jugador segun la accion
         if accion == 'abajo':
-            nueva_coord = (jugador[0] + 1, jugador[1])
+            nueva_coordenada = (jugador[0] + 1, jugador[1])
         elif accion == 'arriba':
-            nueva_coord = (jugador[0] - 1, jugador[1])
+            nueva_coordenada = (jugador[0] - 1, jugador[1])
         elif accion == 'derecha':
-            nueva_coord = (jugador[0], jugador[1] + 1)
+            nueva_coordenada = (jugador[0], jugador[1] + 1)
         elif accion == 'izquierda':
-            nueva_coord = (jugador[0], jugador[1] - 1)
+            nueva_coordenada = (jugador[0], jugador[1] - 1)
 
-        return nueva_coord
+        return nueva_coordenada
 
+    def lista_a_tupla(lista):
+        return tuple(lista)
 
+    # listado coordenadas cajas (tupla de tuplas),  posicion jugador, cantidad de movimientos máximos)
+    INITIAL = (lista_a_tupla(cajas), jugador, maximos_movimientos)
 
-    def list_to_tuple(lst):
-        return tuple(lst)
-
-
-    def tuple_to_list(tup):
-        return [coord for coord in tup]
-
-
-    # listado coordenadas cajas,  posicion jugador, cantidad de movimientos máximos)
-    INITIAL = (list_to_tuple(cajas), jugador, maximos_movimientos)
     class SokobanProblem(SearchProblem):
         def cost(self, state1, action, state2):
             return 1
@@ -112,7 +98,7 @@ def jugar(paredes, cajas, objetivos, jugador, maximos_movimientos):
             # Siempre y cuando los maximos movientos no se hayan alcanzado
 
             for caja in boxes:
-                if caja not in OBJETIVOS:
+                if caja not in objetivos:
                     return False
 
             if mov_restantes < 0:
@@ -126,7 +112,7 @@ def jugar(paredes, cajas, objetivos, jugador, maximos_movimientos):
 
             for accion in ACTIONS:
 
-                if movimientoValido(jugador, accion, boxes):
+                if movimiento_valido(jugador, accion, boxes):
                     acciones_disponibles.append(accion)
 
             return tuple(acciones_disponibles)
@@ -135,7 +121,7 @@ def jugar(paredes, cajas, objetivos, jugador, maximos_movimientos):
         def result(self, state, action):
             boxes, jugador, mov_restantes = state
 
-            new_jugador_coord = nuevaCoord(jugador,action)
+            new_jugador_coord = nueva_coordenada(jugador,action)
 
             mov_restantes = mov_restantes -1
 
@@ -146,12 +132,12 @@ def jugar(paredes, cajas, objetivos, jugador, maximos_movimientos):
                 for caja in boxes:
                     new_caja_coord = caja
                     if new_jugador_coord == caja:
-                        new_caja_coord = nuevaCoord(caja,action)
+                        new_caja_coord = nueva_coordenada(caja,action)
                     new_boxes.append(new_caja_coord)
             else:
                 new_boxes = boxes
 
-            return (list_to_tuple(new_boxes), new_jugador_coord, mov_restantes)
+            return (lista_a_tupla(new_boxes), new_jugador_coord, mov_restantes)
 
                         
 
@@ -159,24 +145,26 @@ def jugar(paredes, cajas, objetivos, jugador, maximos_movimientos):
             boxes, jugador, mov_restantes = state
             #La heuristica sera suma de la distancia manhattan de cada caja con su objetivo mas cercano
             distancia = 0
-            boxes = list_to_tuple(boxes)
+            boxes = lista_a_tupla(boxes)
             for caja in boxes:
-                distancia_caja = distanciaObjetivoCercano(caja)
+                distancia_caja = distancia_objetivo_cercano(caja)
                 distancia += distancia_caja
             return distancia
 
     if __name__ == "__main__":
         viewer = BaseViewer()
-        result = ()
         result = astar(SokobanProblem(INITIAL), graph_search=True, viewer=viewer)
 
         if result is not None:
-            pasos=[]
+            print("Estado meta:")
+            print(result.state)
+
+            secuencia=[]
             for action, state in result.path():
                 if action is not None:
                     print(action)
                     
-            return pasos 
+            return secuencia 
 
         else:
             print("No se encontró una solución para el problema de búsqueda.")
